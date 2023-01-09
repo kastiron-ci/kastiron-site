6511e49fe3d4bfc384f8ec5139ca19cf25e8f8a3
Tyler Witlin <tyler@codezero.io>
2023-01-08 22:40:19 -0500

    chore: update prettier rules
    Signed-off-by: Tyler Witlin <tyler@codezero.io>

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,27 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	onwarn: (warning, handler) => {
-		if (warning.code.startsWith('a11y-')) {
-			return;
-		}
-		handler(warning);
-	},
+  onwarn: (warning, handler) => {
+    if (warning.code.startsWith('a11y-')) {
+      return
+    }
+    handler(warning)
+  },
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false
-		}),
-	}
-};
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+    }),
+  },
+}
 
-export default config;
+export default config
